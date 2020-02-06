b3fddc9cd794eaa53b2809310cafc2b51a866f07
lwhay <lwh@whu.edu.cn>
2020-02-06 13:24:25 +0800

    Add connectclustering

---
+++ src/main/java/ics/whu/edu/cn/madrix/clustering/connect/Transitive.java
@@ -0,0 +1,99 @@
+/**
+ *
+ */
+package ics.whu.edu.cn.madrix.clustering.connect;
+
+import ics.whu.edu.cn.madrix.clustering.density.AbstractDensityClustering;
+import ics.whu.edu.cn.madrix.clustering.density.IClustering;
+import ics.whu.edu.cn.madrix.stream.utils.OrderInformation;
+
+import java.util.*;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Transitive extends AbstractDensityClustering implements IClustering {
+    private Map<Integer, List<Integer>> distSortedNbs = new HashMap<>();
+
+    private Map<Integer, Set<Integer>> directTo = new HashMap<>();
+
+    private final double[][] dists;
+
+    private Map<Integer, Integer> labels = new HashMap<>();
+
+    private Map<Integer, Integer> nbCnts = new HashMap<>();
+
+    public Transitive(double[][] data, double cutoff, boolean isDTW) {
+        this.isDTW = isDTW;
+        this.data = data;
+        this.cutoff = cutoff;
+        rankingdist = new double[data.length * data.length];
+        dists = new double[data.length][];
+        for (int i = 0; i < data.length; i++) {
+            dists[i] = new double[data.length];
+            for (int j = 0; j < data.length; j++) {
+                dists[i][j] = distance(data[i], data[j], isDTW);
+                rankingdist[i * data.length + j] = dists[i][j];
+            }
+        }
+        Arrays.sort(rankingdist);
+        radius = rankingdist[(int) (rankingdist.length * cutoff)];
+        for (int i = 0; i < data.length; i++) {
+            Map<Integer, Double> distMap = new HashMap<>();
+            for (int j = 0; j < data.length; j++) {
+                distMap.put(j, dists[i][j]);
+            }
+            distSortedNbs.put(i, OrderInformation.keySortByValue(distMap, true));
+            int nCnt = 0;
+            for (int j = 0; j < data.length; j++) {
+                if (dists[i][distSortedNbs.get(i).get(j)] > radius) {
+                    break;
+                } else {
+                    if (!directTo.containsKey(i)) {
+                        directTo.put(i, new HashSet<>());
+                    }
+                    directTo.get(i).add(distSortedNbs.get(i).get(j));
+                }
+                nCnt++;
+            }
+            nbCnts.put(i, nCnt);
+        }
+    }
+
+    @Override
+    public int[] export() {
+        int[] results = new int[labels.size()];
+        for (int i = 0; i < labels.size(); i++) {
+            results[i] = labels.get(i);
+        }
+        return results;
+    }
+
+    private Set<Integer> reached = new HashSet<>();
+
+    @Override
+    public void action() {
+        int cid = 1;
+        for (int i = 0; i < data.length; i++) {
+            Set<Integer> related = new HashSet<>();
+            if (reached.contains(i)) {
+                //labels.put(i, labels.get(derivedFrom.get(i)));
+            } else {
+                iterExp(i, cid++, related);
+            }
+        }
+
+    }
+
+    private void iterExp(int id, int cid, Set<Integer> related) {
+        related.add(id);
+        labels.put(id, cid);
+        reached.add(id);
+        for (Integer p : directTo.get(id)) {
+            if (!reached.contains(p)) {
+                iterExp(p, cid, related);
+            }
+        }
+    }
+}
---
+++ src/test/java/ics/whu/edu/cn/madrix/clustering/density/ConntectTest.java
@@ -0,0 +1,62 @@
+package ics.whu.edu.cn.madrix.clustering.density;
+
+import ics.whu.edu.cn.madrix.clustering.connect.Transitive;
+import ics.whu.edu.cn.madrix.clustering.evaluation.ClusteringMetrics;
+import ics.whu.edu.cn.madrix.clustering.wapper.ClusteringBenchTranslator;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class ConntectTest {
+    /**
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        if (args.length != 3) {
+            System.out.println("Usage: command path cutoff isDTW");
+            System.exit(-1);
+        }
+        ClusteringBenchTranslator cbt = new ClusteringBenchTranslator(args[0]);
+        Transitive ds = new Transitive(cbt.getData(), Double.parseDouble(args[1]), Boolean.parseBoolean(args[2]));
+        ds.action();
+        int[] labels = ds.export();
+        Map<Integer, Integer> ccnt = new HashMap<>();
+        for (int i = 0; i < labels.length; i++) {
+            if (ccnt.containsKey(labels[i])) {
+                ccnt.put(labels[i], ccnt.get(labels[i]) + 1);
+            } else {
+                ccnt.put(labels[i], 1);
+            }
+        }
+        Set<Integer> output = new HashSet<>();
+        for (Map.Entry<Integer, Integer> pair : ccnt.entrySet()) {
+            if (pair.getValue() >= 3)
+                output.add(pair.getKey());
+        }
+        Map<Integer, Integer> labelMap = new HashMap<>();
+        for (int i = 0; i < labels.length; i++) {
+            if (ccnt.get(labels[i]) >= 3) {
+                labelMap.put(i, labels[i]);
+            } else {
+                labelMap.put(i, -1);
+            }
+        }
+        Map<Integer, Integer> groundTruth = new HashMap<>();
+        int idx = 0;
+        for (int label : cbt.getLabels()) {
+            groundTruth.put(idx++, label);
+        }
+        for (int i = 0; i < labels.length; i++) {
+            if (output.contains(labels[i]))
+                System.out.println(cbt.getData()[i][0] + "\t" + cbt.getData()[i][1] + "\t" + labels[i]);
+        }
+        ClusteringMetrics cm = new ClusteringMetrics(groundTruth, labelMap);
+        System.out.println("Cutoff: " + ds.getCutoff() + " Radius: " + ds.getRadius() + " MaxDist: "
+                + ds.getMaximalDist() + "\nGenerated: " + output.size() + " Acc: " + cm.getAcc() + " Ari: "
+                + cm.getAri() + " Ami: " + cm.getAmi());
+    }
+}
